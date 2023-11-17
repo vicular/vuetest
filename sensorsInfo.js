06b469011cf96c61c3cee48984916169d35f0811
vicular <mail@vicular.com>
2023-11-17 17:39:01 +0800

    mod sth

---
+++ sensorsInfo.js
@@ -0,0 +1,491 @@
+export default {
+    'common': {
+        pj_name: '智能条件单'
+    },
+    'web-stay': [
+        {
+            'index.html': {
+                page_title: '条件单首页_使用攻略_策略名称',
+            },
+            'detail.html': {
+                page_title: '条件单首页_我的条件单_条件单详情页',
+            }
+        }
+    ],
+    '1-1': {
+        page_title: '条件单首页',
+        button_name: '立即查看使用攻略'
+    },
+    '1-1-1': {
+        page_title: '条件单首页_使用攻略',
+        button_name: 'button_name'
+    },
+    '1-2': {
+        page_title: '条件单首页',
+        button_name: '查看更多',
+        button_position: '我的条件单'
+    },
+    '1-3': {
+        page_title: '条件单首页',
+        button_name: '今日已触发',
+        button_position: '我的条件单'
+    },
+    '1-4': {
+        page_title: '条件单首页',
+        button_name: '实时监控中',
+        button_position: '我的条件单'
+    },
+    '1-5': {
+        page_title: '条件单首页',
+        button_name: '风控设置',
+        button_position: '我的条件单'
+    },
+    '1-6': {
+        page_title: '条件单首页_我的条件单',
+        button_name: 'button_name'
+    },
+    '1-6-1': {
+        page_title: '条件单首页_我的条件单',
+        work_title: '点击事件：策略名称_股票名称'
+    },
+    '1-6-2-1': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-修改',
+        work_id: '点击事件：定价买入-管理条件单-修改',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-2-2': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-终止',
+        work_id: '点击事件：定价买入-管理条件单-终止',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-3-1': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-修改',
+        work_id: '点击事件：定价卖出-管理条件单-修改',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-3-2': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-终止',
+        work_id: '点击事件：定价卖出-管理条件单-终止',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-4-1': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-修改',
+        work_id: '点击事件：日涨跌幅-管理条件单-修改',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-4-2': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-终止',
+        work_id: '点击事件：日涨跌幅-管理条件单-终止',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-5-1': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-修改',
+        work_id: '点击事件：止盈止损-管理条件单-修改',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-5-2': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-终止',
+        work_id: '点击事件：止盈止损-管理条件单-终止',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-6-1': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-修改',
+        work_id: '点击事件：定时买卖-管理条件单-修改',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-6-2': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-终止',
+        work_id: '点击事件：定时买卖-管理条件单-终止',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-7-1': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-修改',
+        work_id: '点击事件：反弹买入-管理条件单-修改',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-7-2': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-终止',
+        work_id: '点击事件：反弹买入-管理条件单-终止',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-8-1': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-修改',
+        work_id: '点击事件：回落卖出-管理条件单-修改',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-8-2': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-终止',
+        work_id: '点击事件：回落卖出-管理条件单-终止',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-9-1': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-修改',
+        work_id: '点击事件：网格交易-管理条件单-修改',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-9-2': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-终止',
+        work_id: '点击事件：网格交易-管理条件单-终止',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-10-1': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-修改',
+        work_id: '点击事件：智能国债逆回购-管理条件单-修改',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-10-2': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-终止',
+        work_id: '点击事件：智能国债逆回购-管理条件单-终止',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-11-1': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-修改',
+        work_id: '点击事件：开板卖出-管理条件单-修改',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-11-2': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-终止',
+        work_id: '点击事件：开板卖出-管理条件单-终止',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-12-1': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-修改',
+        work_id: '点击事件：涨停买卖-管理条件单-修改',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-6-12-2': {
+        page_title: '条件单首页_我的条件单_条件单详情页',
+        work_title: '点击事件：管理条件单-终止',
+        work_id: '点击事件：涨停买卖-管理条件单-终止',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-7': {
+        page_title: '条件单首页',
+        button_name: '智能打新',
+        button_position: '打新'
+    },
+    '1-7-1': {
+        page_title: '条件单首页_智能打新',
+        button_name: '立即预约',
+        button_position: '新股申购',
+        request_param: 'request_param'
+    },
+    '1-7-2': {
+        page_title: '条件单首页_智能打新',
+        button_name: '立即预约',
+        button_position: '新债申购',
+        request_param: 'request_param'
+    },
+    '1-7-3': {
+        page_title: '条件单首页_智能打新',
+        button_name: '取消选中预约',
+        button_position: '我的预约',
+        request_param: 'request_param'
+    },
+    '1-8': {
+        page_title: '条件单首页',
+        button_name: '开板卖出',
+        button_position: '打新'
+    },
+    '1-8-1': {
+        page_title: '条件单首页_开板卖出',
+        button_name: '确定下单',
+        request_param: 'request_param'
+    },
+    '1-8-1-1': {
+        page_title: '条件单首页_开板卖出_策略二次确认弹窗',
+        work_title: '确定',
+        work_id: '点击事件：开板卖出-确定',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-8-1-2': {
+        page_title: '条件单首页_开板卖出_策略二次确认弹窗',
+        work_title: '取消',
+        work_id: '点击事件：开板卖出-取消',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-9-1': {
+        page_title: '条件单首页_智能国债逆回购',
+        button_name: '确定下单',
+        request_param: 'request_param'
+    },
+    '1-9-1-1': {
+        page_title: '条件单首页_智能国债逆回购_策略二次确认弹窗',
+        work_title: '确定',
+        work_id: '点击事件：智能国债逆回购-确定',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-9-1-2': {
+        page_title: '条件单首页_智能国债逆回购_策略二次确认弹窗',
+        work_title: '取消',
+        work_id: '点击事件：智能国债逆回购-取消',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-10': {
+        page_title: '条件单首页',
+        button_name: '定价买入',
+        button_position: '买入'
+    },
+    '1-10-1': {
+        page_title: '条件单首页_定价买入',
+        button_name: '确定下单',
+        request_param: 'request_param'
+    },
+    '1-10-1-1': {
+        page_title: '条件单首页_定价买入_策略二次确认弹窗',
+        work_title: '确定',
+        work_id: '点击事件：定价买入-确定',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-10-1-2': {
+        page_title: '条件单首页_定价买入_策略二次确认弹窗',
+        work_title: '取消',
+        work_id: '点击事件：定价买入-取消',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-11': {
+        page_title: '条件单首页',
+        button_name: '反弹买入',
+        button_position: '买入'
+    },
+    '1-11-1': {
+        page_title: '条件单首页_反弹买入',
+        button_name: '确定下单',
+        request_param: 'request_param'
+    },
+    '1-11-1-1': {
+        page_title: '条件单首页_反弹买入_策略二次确认弹窗',
+        work_title: '确定',
+        work_id: '点击事件：反弹买入-确定',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-11-1-2': {
+        page_title: '条件单首页_反弹买入_策略二次确认弹窗',
+        work_title: '取消',
+        work_id: '点击事件：反弹买入-取消',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-12': {
+        page_title: '条件单首页',
+        button_name: '定价卖出',
+        button_position: '卖出'
+    },
+    '1-12-1': {
+        page_title: '条件单首页_定价卖出',
+        button_name: '确定下单',
+        request_param: 'request_param'
+    },
+    '1-12-1-1': {
+        page_title: '条件单首页_定价卖出_策略二次确认弹窗',
+        work_title: '确定',
+        work_id: '点击事件：定价卖出-确定',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-12-1-2': {
+        page_title: '条件单首页_定价卖出_策略二次确认弹窗',
+        work_title: '取消',
+        work_id: '点击事件：定价卖出-取消',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-13': {
+        page_title: '条件单首页',
+        button_name: '回落卖出',
+        button_position: '卖出'
+    },
+    '1-13-1': {
+        page_title: '条件单首页_回落卖出',
+        button_name: '确定下单',
+        request_param: 'request_param'
+    },
+    '1-13-1-1': {
+        page_title: '条件单首页_回落卖出_策略二次确认弹窗',
+        work_title: '确定',
+        work_id: '点击事件：回落卖出-确定',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-13-1-2': {
+        page_title: '条件单首页_回落卖出_策略二次确认弹窗',
+        work_title: '取消',
+        work_id: '点击事件：回落卖出-取消',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-14': {
+        page_title: '条件单首页',
+        button_name: '止盈止损',
+        button_position: '卖出'
+    },
+    '1-14-1': {
+        page_title: '条件单首页_止盈止损',
+        button_name: '确定下单',
+        request_param: 'request_param'
+    },
+    '1-14-1-1': {
+        page_title: '条件单首页_止盈止损_策略二次确认弹窗',
+        work_title: '确定',
+        work_id: '点击事件：止盈止损-确定',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-14-1-2': {
+        page_title: '条件单首页_止盈止损_策略二次确认弹窗',
+        work_title: '取消',
+        work_id: '点击事件：止盈止损-取消',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-15': {
+        page_title: '条件单首页',
+        button_name: '日涨跌幅',
+        button_position: '双向'
+    },
+    '1-15-1': {
+        page_title: '条件单首页_日涨跌幅',
+        button_name: '确定下单',
+        request_param: 'request_param'
+    },
+    '1-15-1-1': {
+        page_title: '条件单首页_日涨跌幅_策略二次确认弹窗',
+        work_title: '确定',
+        work_id: '点击事件：日涨跌幅-确定',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-15-1-2': {
+        page_title: '条件单首页_日涨跌幅_策略二次确认弹窗',
+        work_title: '取消',
+        work_id: '点击事件：日涨跌幅-取消',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-16': {
+        page_title: '条件单首页',
+        button_name: '网格交易',
+        button_position: '双向'
+    },
+    '1-16-1': {
+        page_title: '条件单首页_网格交易',
+        button_name: '确定下单',
+        request_param: 'request_param'
+    },
+    '1-16-1-1': {
+        page_title: '条件单首页_网格交易_策略二次确认弹窗',
+        work_title: '确定',
+        work_id: '点击事件：网格交易-确定',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-16-1-2': {
+        page_title: '条件单首页_网格交易_策略二次确认弹窗',
+        work_title: '取消',
+        work_id: '点击事件：网格交易-取消',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-17': {
+        page_title: '条件单首页',
+        button_name: '定时买卖',
+        button_position: '双向'
+    },
+    '1-17-1': {
+        page_title: '条件单首页_定时买卖',
+        button_name: '确定下单',
+        request_param: 'request_param'
+    },
+    '1-17-1-1': {
+        page_title: '条件单首页_定时买卖_策略二次确认弹窗',
+        work_title: '确定',
+        work_id: '点击事件：定时买卖-确定',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-17-1-2': {
+        page_title: '条件单首页_定时买卖_策略二次确认弹窗',
+        work_title: '取消',
+        work_id: '点击事件：定时买卖-取消',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-18': {
+        page_title: '条件单首页',
+        button_name: '涨停买卖',
+        button_position: '双向'
+    },
+    '1-18-1': {
+        page_title: '条件单首页_涨停买卖',
+        button_name: '确定下单',
+        request_param: 'request_param'
+    },
+    '1-18-1-1': {
+        page_title: '条件单首页_涨停买卖_策略二次确认弹窗',
+        work_title: '确定',
+        work_id: '点击事件：涨停买卖-确定',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-18-1-2': {
+        page_title: '条件单首页_涨停买卖_策略二次确认弹窗',
+        work_title: '取消',
+        work_id: '点击事件：涨停买卖-取消',
+        button_name: 'button_name',
+        request_param: 'request_param'
+    },
+    '1-19-1': {
+        page_title: '条件单首页-风控设置',
+        button_name: '保存'
+    },
+}
